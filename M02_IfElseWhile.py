214a0b8d7d797f0e821a9aafebc6d36a21cf0f5b
Leniusel <112113603+Leniusel@users.noreply.github.com>
2022-08-31 18:17:15 -0700

    Create M02_IfElseWhile.py
    M02 Lab Assignment: Case Study: if...else and while
    
    Description:
    Write a Python app that will accept student names and GPAs and test if the student qualifies for either the Dean's List or the Honor Roll. Your app will:
    ask for and accept a student's last name.
    quit processing student records if the last name entered is 'ZZZ'.
    ask for and accept a student's first name.
    ask for and accept the student's GPA as a float.
    test if the student's GPA is 3.5 or greater and, if so, print a message saying that the student has made the Dean's List.
    test if the student's GPA is 3.25 or greater and, if so, print a message saying that the studnet has made the Honor Roll.
    Test your app using at least five students.
    Your header comments need to contain
    Your name
    The file name for your app
    A brief description of what your app will do

---
+++ M02_IfElseWhile.py
@@ -0,0 +1,54 @@
+#Name: Doris Tran
+#Filename: M02_IfElseWhile.py
+#The App will accept inputs for student's name, their gpa, and will give different messages depending on their gpa. The user
+#can exit the app by inputing 'ZZZ' as a student's lastname.
+
+#Default variables
+student = " "      #stores student's fullname
+GPA = 0.0          #stores gpa score
+process = True     #boolean value that checks if the app is processing student records
+
+lname = ""         #stores student's lastname
+fname = ""         #stores student's firstname
+
+#Functions that returns a student's fullname and checks if the user wants to exit the app via entering "ZZZ" as a student's lastname.
+def fullname(student, process):
+    lname = input("\nEnter Student's Last Name (Enter 'ZZZ' to quit): ")
+    if lname == "ZZZ":
+        print("Quiting.....") 
+        process = False
+        return student, process
+    else:
+        fname = input("Enter Student's First Name: ")
+        student = fname + " " + lname
+        return student, process
+   
+ #Function that validates the GPA input. It checks if the input is a float. If the user inputs invalid data, ask user to try again.   
+def gpa_score(GPA):
+    try:
+        GPA = float(input("\nEnter " + student + "'s GPA: "))
+        assert 4.0 >= GPA >= 0.0
+        return GPA;
+    except:
+        print("Please enter a valid GPA.")
+        return gpa_score(GPA);
+
+#Function that prints a message depending on the student's gpa. Python checks row by row, so looking for students that qualify
+#for the Dean's list goes first. Honar Roll goes second.
+def message():
+    if GPA >= 3.50:
+        print("The student " + student + " has made the Dean's List.")
+    elif GPA >= 3.25:
+        print("The student " + student + " has made the Honar Roll.")
+    else:
+        print("This student did not make it to the Dean's List or the Honar Roll.")
+
+#A while loop that checks if the process variable is true. Once false, the app will terminate
+while process == True:
+    student, process = fullname(student, process)
+    if process == False:
+        print("program terminated")
+    else:  
+      GPA = gpa_score(GPA)
+      print(GPA)
+      message()
